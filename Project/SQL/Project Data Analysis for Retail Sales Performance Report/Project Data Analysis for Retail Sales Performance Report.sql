77fe8436a9adc2eff22f61dd72ed0d2ddb8611a0
Muhammad Yusuf Aristyanto <aristyanto2320@gmail.com>
2021-01-28 12:26:00 +0700

    Change SQL File

--- Project/SQL/Project Data Analysis for Retail Sales Performance Report/Project Data Analysis for Retail Sales Performance Report.sql
+++ Project/SQL/Project Data Analysis for Retail Sales Performance Report/Project Data Analysis for Retail Sales Performance Report.sql
@@ -4,16 +4,12 @@ USE dqlab;
 SELECT
     YEAR(order_date) AS years,
     ROUND(SUM(sales), 2) AS sales,
-    COUNT(order_quantity) AS number_of_order
+    COUNT(order_id) AS number_of_order
 FROM
     dqlab_sales_store
 WHERE
-    YEAR(order_date) BETWEEN 2009
-    AND 2012
-    AND order_status LIKE 'Order Finished'
+    order_status = 'Order Finished'
 GROUP BY
-    years
-ORDER BY
     years;
 
 -- Overall Performance by Product Sub Category
